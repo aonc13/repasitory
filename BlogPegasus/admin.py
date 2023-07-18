52f30e5a1ffa3ecffdb45065acb385389b779c9a
shayanm7 <7shayanm7@gmail.com>
2023-07-17 23:16:09 -0700

    cp coment

--- BlogPegasus/admin.py
+++ BlogPegasus/admin.py
@@ -1,17 +1,46 @@
-from django.contrib import admin
+from django import forms
 from django.forms import Textarea
-from .models import TexTak
+from .models import TexTak, menuItem
+from django.contrib import admin
 
-@admin.register(TexTak)
+class TexTakForm(forms.ModelForm):
+    class Meta:
+        model = TexTak
+        fields = '__all__'
+        widgets = {
+            'mtn1': Textarea(attrs={'style': 'width: 400px; height: 200px; font-size: 4vh; writing-mode: vertical-lr; text-orientation: mixed; unicode-bidi: bidi-override; direction: rtl; line-break: normal'}),
+            'mtn2': Textarea(attrs={'style': 'width: 400px; height: 200px; font-size: 4vh; writing-mode: vertical-lr; text-orientation: mixed; unicode-bidi: bidi-override; direction: rtl; line-break: normal'}),
+            'mtn3': Textarea(attrs={'style': 'width: 400px; height: 200px; font-size: 4vh; writing-mode: vertical-lr; text-orientation: mixed; unicode-bidi: bidi-override; direction: rtl; line-break: normal'}),
+        }
+
 class TexTakAdmin(admin.ModelAdmin):
-    list_display = ['mtn1', 'mtn2']
+    list_display = ['mtn1', 'mtn2', 'mtn3']
+    form = TexTakForm
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        form.base_fields['mtn1'].widget = Textarea(attrs={
-            'style': 'width: 400px; height: 200px;font-size: 4vh; writing-mode: vertical-lr; text-orientation: mixed; unicode-bidi: bidi-override; direction: rtl; line-break: normal'
-        })
-        form.base_fields['mtn2'].widget = Textarea(attrs={
-            'style': 'width: 400px;font-size: 4vh; height: 200px; writing-mode: vertical-lr; text-orientation: mixed; unicode-bidi: bidi-override; direction: rtl; line-break: normal'
-        })
-        return form
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['title'] = 'ویرایش مدل'
+        extra_context['is_edit'] = True
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+
+class menuItemForm(forms.ModelForm):
+    class Meta:
+        model = menuItem
+        fields = '__all__'
+
+
+
+class menuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'mtn_kol', 'lnk_a']
+    form = TexTakForm
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['title'] = 'ویرایش مدل'
+        extra_context['is_edit'] = True
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
+
+
+admin.site.register(TexTak, TexTakAdmin)
+admin.site.register(menuItem, menuItemAdmin)
